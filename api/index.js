f4c2d5c59990a4dbb1b4f08c2058415797446f07
harshtarang7 <tarangharsh7@gmail.com>
2024-06-23 16:42:31 +0530

    added mongoose

--- api/index.js
+++ api/index.js
@@ -1,4 +1,11 @@
 import express from 'express'
+import mongoose from 'mongoose';
+
+mongoose.connect("mongodb://localhost:27017/user-auth")
+.then(()=>console.log('connected to mongo db'))
+.catch((err)=>{
+    console.log(err)
+})
 
 const app = express();
 

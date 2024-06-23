33cd65d84ace9e932ca42cf80e7bdba4216ad507
harshtarang7 <tarangharsh7@gmail.com>
2024-06-23 16:59:25 +0530

    added routes

--- api/index.js
+++ api/index.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import mongoose from 'mongoose';
+import router from './routes/user.route.js';
 
 mongoose.connect("mongodb://localhost:27017/user-auth")
 .then(()=>console.log('connected to mongo db'))
@@ -7,7 +8,11 @@ mongoose.connect("mongodb://localhost:27017/user-auth")
     console.log(err)
 })
 
+
 const app = express();
+
+
+app.use('/api/user',router)
 
 app.listen(3000,()=>{
     console.log("Server is running on port 3000");
